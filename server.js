1810ab7030deb5a114a955ad2059795d118f8e90
vetrivelj <vetrivel.jothiprakasam@gmail.com>
2019-01-30 00:44:11 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 var Input,Output,Webservice; 
-var Output = require("./output);
+var Output = require("./output");
 var SendResponse = require("./sendResponse");
 
 var qString = "";
